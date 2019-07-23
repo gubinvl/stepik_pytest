b1687976fff6e70d9936ffd867266a081b967f6e
gubinvl <gubinvl@users.noreply.github.com>
2019-07-23 16:45:09 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,9 +1,5 @@
-link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_guest_should_be_btn_add_to_basket(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
-
-def test_guest_should_see_login_link_fail(browser):
-    browser.get(link)
-    browser.find_element_by_css_selector("#magic_link")
+    browser.find_element_by_css_selector(".btn-add-to-basket")
